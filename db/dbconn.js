d74ab73afd60510c69877f800d88f842747d98e3
Michle99 <sammiyonyx@gmail.com>
2023-11-17 17:21:57 -0500

    Added validation rules to mongodb movies collections

--- db/dbconn.js
+++ db/dbconn.js
@@ -6,6 +6,30 @@ const connectionString = process.env.MONGO_URI || "";
 
 const client = new MongoClient(connectionString);
 
+// Define validation rules for the db
+const validationRules = {
+  $jsonSchema: {
+    bsonType: "object",
+    required: ["plot", "genres", "type", "title", "poster", "cast", "fullplot", "runtime"],
+    properties: {
+      plot: { bsonType: "string" },
+      genres: {
+        bsonType: "array",
+        items: { bsonType: "string" }
+      },
+      type: { bsonType: "string" },
+      title: { bsonType: "string" },
+      poster: { bsonType: "string" },
+      cast: {
+        bsonType: "array",
+        items: { bsonType: "string" }
+      },
+      fullplot: { bsonType: "string" },
+      runtime: { bsonType: "int" }
+    }
+  }
+};
+
 const connectToDatabase = async () => {
   try {
     await client.connect();
@@ -16,5 +40,17 @@ const connectToDatabase = async () => {
     console.error("Error connecting to MongoDB:", error);
   }
 };
+
+try {
+  // Apply the validation rules to the grades collection
+  await db.command({
+    collMod: "movies",
+    validator: validationRules,
+    validationAction: "warn",
+  });
+  console.log("Validation rules applied successfully.");
+} catch (error) {
+  console.error("Error applying validation rules:", error);
+}
 
 export default connectToDatabase;
