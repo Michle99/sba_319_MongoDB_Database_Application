52b14402881f73a29a6807c2efa6980497bb7d1d
Michle99 <sammiyonyx@gmail.com>
2023-11-17 15:26:48 -0500

    Added loadEnv.js to envs, connectToDatabase function in dbconn.js

---
+++ db/dbconn.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import '../loadEnv.js'
+
+
+const connectionString = process.env.MONGO_URI || "";
+
+const client = new MongoClient(connectionString);
+
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const db = client.db("sample_restaurants");
+    return db;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+export default connectToDatabase;
