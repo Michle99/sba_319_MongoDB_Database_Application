6a9ace52f5585ff853567123f51abb582b7bab3d
Michle99 <sammiyonyx@gmail.com>
2023-11-17 15:42:44 -0500

    Defined GET routes in routes/movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -4,3 +4,18 @@ import { ObjectId } from 'mongodb';
 
 const router = express.Router();
 
+/*********************************************/
+//              GET ALL RESTAURANTS
+/*********************************************/
+router.get('/', async (req, res) => {
+    try {
+       const db = await connectToDatabase();
+       let collection = db.collection("movies");
+       console.log("Movies collection:", collection);
+       let results = await collection.find({}).limit(5).toArray();
+       res.send(results).status(200);
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
