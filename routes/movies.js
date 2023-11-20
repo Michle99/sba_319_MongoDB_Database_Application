293aa0272eac509e39863bf6d998340de65f7770
Michle99 <sammiyonyx@gmail.com>
2023-11-20 12:28:23 -0500

    Performed Naive testing on routers and moviesController and works as expected.

--- routes/movies.js
+++ routes/movies.js
@@ -1,88 +1,98 @@
 import express from 'express';
-import connectToDatabase from '../db/dbconn.js';
-import { ObjectId } from 'mongodb';
+import * as moviesController from '../controllers/moviesController.js';
+
 
 const router = express.Router();
 
 /*********************************************/
 //              GET ALL MOVIES
 /*********************************************/
-router.get('/', async (req, res) => {
-    try {
-       const db = await connectToDatabase();
-       let collection = db.collection("movies");
-    //    console.log("Movies collection:", collection);
-       let results = await collection.find({}).limit(5).toArray();
-       res.send(results).status(200);
-    } catch (error) {
-        console.error("Error fetching movies:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
+router.get('/', moviesController.getAllMovies);
+
+// router.get('/', async (req, res) => {
+//     try {
+//        const db = await connectToDatabase();
+//        let collection = db.collection("movies");
+//     //    console.log("Movies collection:", collection);
+//        let results = await collection.find({}).limit(5).toArray();
+//        res.send(results).status(200);
+//     } catch (error) {
+//         console.error("Error fetching movies:", error);
+//         res.status(500).send("Internal Server Error");
+//     }
+// });
 
 
 /*********************************************/
 //              GET MOVIES BY ID
 /*********************************************/
-router.get('/:id', async (req, res) => {
-    const db = await connectToDatabase();
-    let collection = db.collection("movies");
-    let query = { _id: new ObjectId(req.params.id) };
-    let result = await collection.findOne(query);
+router.get('/:id', moviesController.getMovieById);
 
-    if (!result) res.send("Movie Not found").status(404);
-    else res.send(result).status(200);
-})
+// router.get('/:id', async (req, res) => {
+//     const db = await connectToDatabase();
+//     let collection = db.collection("movies");
+//     let query = { _id: new ObjectId(req.params.id) };
+//     let result = await collection.findOne(query);
+
+//     if (!result) res.send("Movie Not found").status(404);
+//     else res.send(result).status(200);
+// })
 
 
 /*********************************************/
 //              POST NEW MOVIE 
 /*********************************************/
-router.post('/', async (req, res) => {
-    const db = await connectToDatabase();
-    let collection = db.collection("movies");
-    let newMovie = req.body;
-    newMovie.date = new Date();
-    let result = await collection.insertOne(newMovie);
-    res.send(result).status(204);
-})
+router.post('/', moviesController.createMovie);
+
+// router.post('/', async (req, res) => {
+//     const db = await connectToDatabase();
+//     let collection = db.collection("movies");
+//     let newMovie = req.body;
+//     newMovie.date = new Date();
+//     let result = await collection.insertOne(newMovie);
+//     res.send(result).status(204);
+// })
 
 
 /*********************************************/
 //              UPDATE MOVIE BY ID
 /*********************************************/
-router.put("/:id", async (req, res) => {
-    try {
-        const query = { _id: new ObjectId(req.params.id) };
-        const updates = {
-        $set: { type: req.body.type },
-        // $push: { genres: req.body.genres },
-        // $push: { genres: { $each:  req.body.genres } }
-        // $currentDate: { lastModified: true }
-        };
-        const db = await connectToDatabase();
-        let collection = db.collection("movies");
-        let result = await collection.updateOne(query, updates);
+router.put('/:id', moviesController.updateMovie);
+
+// router.put("/:id", async (req, res) => {
+//     try {
+//         const query = { _id: new ObjectId(req.params.id) };
+//         const updates = {
+//         $set: { type: req.body.type },
+//         // $push: { genres: req.body.genres },
+//         // $push: { genres: { $each:  req.body.genres } }
+//         // $currentDate: { lastModified: true }
+//         };
+//         const db = await connectToDatabase();
+//         let collection = db.collection("movies");
+//         let result = await collection.updateOne(query, updates);
     
-        res.send(result).status(200);
-    } catch (error) {
-        console.error("Error updating movie data:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
+//         res.send(result).status(200);
+//     } catch (error) {
+//         console.error("Error updating movie data:", error);
+//         res.status(500).send("Internal Server Error");
+//     }
+// });
 
 
 /*********************************************/
 //              DELETE MOVIE BY ID
 /*********************************************/
-router.delete("/:id", async (req, res) => {
-    const query = { _id: new ObjectId(req.params.id) };
+router.delete('/:id', moviesController.deleteMovie);
 
-    const db = await connectToDatabase();
-    const collection = db.collection("movies");
-    let result = await collection.deleteOne(query);
+// router.delete("/:id", async (req, res) => {
+//     const query = { _id: new ObjectId(req.params.id) };
+
+//     const db = await connectToDatabase();
+//     const collection = db.collection("movies");
+//     let result = await collection.deleteOne(query);
   
-    res.send(result).status(200);
-});
+//     res.send(result).status(200);
+// });
 
 export default router;
