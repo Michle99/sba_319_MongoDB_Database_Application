bb2297a9871cd259a423ed2d74df03468dc9c762
Michle99 <sammiyonyx@gmail.com>
2023-11-21 13:18:48 -0500

    Added swaggerOptionConfig for swagger interface.

--- server/createExpressApp.js
+++ server/createExpressApp.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import moviesRouter from '../routes/movies.js';
 import errorHandler from '../middlewares/errorMiddleware.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express'
+import { swaggerOptionsConfig } from '../swagger/swaggerDocs.js';
 
 
 const createExpressApp = () => {
     const app = express();
+    const swaggerSpecs = swaggerJSDoc(swaggerOptionsConfig);
 
     app.use(express.json());
+
+    app.use(
+        '/api-docs', 
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerSpecs)
+    );
+
     app.use('/movies', moviesRouter);
 
     app.use(errorHandler);
--- swagger/swaggerDocs.js
+++ swagger/swaggerDocs.js
@@ -1,4 +1,4 @@
-export const options = {
+export const swaggerOptionsConfig = {
     definition: {
       openapi: "3.1.0",
       info: {
