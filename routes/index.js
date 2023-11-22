2293a51df379ec84d8dcaf4498d4ab641fd3449c
Michle99 <sammiyonyx@gmail.com>
2023-11-22 12:06:32 -0500

    Added and Refactored router.put() to putAMovie router.

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import getAllMoviesRouter from './getAllMovies.js';
 import getSingleMovieRouter from './getSingleMovie.js'
 import postAMovieRouter from './postAMovie.js';
+import putAMovieRouter from './putAMovie.js';
 
 const router = express.Router();
 
 router.use('/movies', getAllMoviesRouter);
 router.use('/movies', getSingleMovieRouter);
 router.use('/movies', postAMovieRouter);
-
+router.use('/movies', putAMovieRouter);
 
 export default router;
